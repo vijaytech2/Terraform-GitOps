65516e2bb8981a814893676cfccb828ddb315bb5
Vijayaragavan C <54120708+vijaytech2@users.noreply.github.com>
2023-07-18 12:36:40 +0530

    Update main.tf
    ud

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # Terraform state will be stored in S3
 terraform {
   backend "s3" {
-    bucket = "senthil-bkt"
+    bucket = "vijay-terra-state-bkt"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
